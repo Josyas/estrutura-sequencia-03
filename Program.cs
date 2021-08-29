5ee075edf67c9a22ea5a1ec66ae568205eb24184
Josias <87546094+Josyas@users.noreply.github.com>
2021-08-29 14:51:50 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+namespace exercicio_estrutura_sequencial_05 {
+    class Program {
+        static void Main(string[] args) {
+            double tot;
+
+
+            string[] v = Console.ReadLine().Split(' ');
+            int qtpeca = int.Parse(v[0]);
+            double vlpaga = double.Parse(v[1]);
+
+            String[] ve = Console.ReadLine().Split(' ');
+            int qtpeca2 = int.Parse(ve[0]);
+            double vlpaga2 = double.Parse(ve[1]);
+
+            tot = vlpaga * qtpeca + vlpaga2 * qtpeca2;
+            Console.WriteLine(tot);
+        }
+    }
+}
